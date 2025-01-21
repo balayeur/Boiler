c462be2c1f1d437af54e6a7eb959cfed6e8baed9
balayeur <balayeur@hotmail.com>
2025-01-21 17:48:31 +0100

    Update SensorFunctions.h

--- SensorFunctions.h
+++ SensorFunctions.h
@@ -6,6 +6,13 @@
 
 extern DallasTemperature sensors;
 
+#define TEMP_ALLER  0
+#define TEMP_RETOUR 1
+#define TEMP_CUVE   2
+// #define TEMP_EAU_CHAUDE 3
+// #define TEMP_EXTERIEUR 4
+
+
 String readDSTemperatureC(int index);
 bool getBurnerState(float _current);
 float getCurrent(int _sensorValue);
